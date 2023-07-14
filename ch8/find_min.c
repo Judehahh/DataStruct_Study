a10f88a346b988ad6f5d1b6c1941262a78ad8486
Judehahh <liujh20121601@live.com>
2023-07-14 07:21:27 +0000

    WangDao 8.3.4 practice 5: find the kth smallest num

--- ch8/find_min.c
+++ ch8/find_min.c
@@ -31,11 +31,32 @@ void quick_sort(int A[], int low, int high) {
     }
 }
 
+int find_min(int A[], int low, int high, int k) {
+    if (k - 1 < low || k - 1 > high) {
+        printf("ERROR: k = %d out of range\n", k);
+        return -1;
+    }
+    int pivot_index = partition(A, low, high);
+    if (pivot_index == k - 1)
+        return A[pivot_index];
+    else if(pivot_index > k - 1)
+        return find_min(A, low, pivot_index - 1, k);
+    else
+        return find_min(A, pivot_index + 1, high, k);
+}
+
 int main() {
 	int array[10] = {9,8,7,6,6,6,5,4,3,3};
+	int tmp[10] = {9,8,7,6,6,6,5,4,3,3};
+    printf("raw array: ");
     printa(array, 10);
-    quick_sort(array, 0, 9);
-    printa(array, 10);
+    quick_sort(tmp, 0, 9);
+    printf("sorted array: ");
+    printa(tmp, 10);
+
+    int k;
+    for (k = 1; k <= 10; ++k)
+        printf("the %d th min in array is: %d\n", k, find_min(array, 0, 9, k));
 
 	return 0;
 }
